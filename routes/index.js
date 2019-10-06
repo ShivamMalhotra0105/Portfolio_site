5285f841a261fc414941a173f5722365e23ad4cc
200394261 <51100164+200394261@users.noreply.github.com>
2019-10-06 17:05:14 -0400

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,27 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* GET home page. */
 router.get('/homepage', function(req, res, next) {
   res.render('homepage', { });
 });
-
+/* GET aboutme page. */
 router.get('/aboutme', function(req, res, next) {
   res.render('aboutme', { });
 });
-
+/* GET services page. */
 router.get('/servicespage', function(req, res, next) {
   res.render('servicespage', { });
 });
-
+/* GET project page. */
 router.get('/projectpage', function(req, res, next) {
   res.render('projectpage', { });
 });
-
+/* GET contact me page. */
 router.get('/contactme', function(req, res, next) {
   res.render('contactme', { });
 });
+
+router.use(express.static('./public/stylesheets/style.css'));
 
 module.exports = router;
