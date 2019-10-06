2d0035b5007970bef80456b0be9bf725c0eb4462
200394261 <51100164+200394261@users.noreply.github.com>
2019-10-06 16:35:27 -0400

    second commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/homepage', function(req, res, next) {
+  res.render('homepage', { });
+});
+
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { });
+});
+
+router.get('/servicespage', function(req, res, next) {
+  res.render('servicespage', { });
+});
+
+router.get('/projectpage', function(req, res, next) {
+  res.render('projectpage', { });
+});
+
+router.get('/contactme', function(req, res, next) {
+  res.render('contactme', { });
+});
+
 module.exports = router;
